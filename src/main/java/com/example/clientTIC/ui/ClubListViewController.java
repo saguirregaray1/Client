0b084c0eb578d2d0dbca0e66400446d63a83832e
Lucas Nieves <tnieves@correo.um.edu.uy>
2022-11-08 13:48:23 -0300

    arreglos graficos

--- src/main/java/com/example/clientTIC/ui/ClubListViewController.java
+++ src/main/java/com/example/clientTIC/ui/ClubListViewController.java
@@ -415,12 +415,22 @@ public class ClubListViewController implements Initializable {
                 horariosBox.getChildren().add(cuposBox);
             }
         }else{throw new Exception("No existe la actividad");}
+
+
     }
 
 
 
 
-
+    @FXML
+    protected void volver(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LogInScreen.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
 
